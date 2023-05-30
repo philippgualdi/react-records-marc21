8496f0ad0935b4c49587fcd9b429c38337368b85
Philipp Gualdi <philipp.gualdi@tugraz.at>
2023-05-30 15:30:41 +0200

    modification: show validation error messages

--- src/lib/Marc21RecordSerializer.js
+++ src/lib/Marc21RecordSerializer.js
@@ -1,6 +1,6 @@
 // This file is part of Invenio.
 //
-// Copyright (C) 2021-2022 Graz University of Technology.
+// Copyright (C) 2021-2023 Graz University of Technology.
 //
 // React-Records-Marc21 is free software; you can redistribute it and/or
 // modify it under the terms of the MIT License; see LICENSE file for more
@@ -106,6 +106,7 @@ export class Marc21RecordSerializer extends DepositRecordSerializer {
                 `metadata.fields.${j}.${keys[4]}`,
                 e.messages.join(" ")
               );
+              break;
             }
             j++;
           }
--- src/lib/components/MetadataFields.js
+++ src/lib/components/MetadataFields.js
@@ -1,13 +1,14 @@
 // This file is part of Invenio.
 //
-// Copyright (C) 2021-2022 Graz University of Technology.
+// Copyright (C) 2021-2023 Graz University of Technology.
 //
 // React-Records-Marc21 is free software; you can redistribute it and/or
 // modify it under the terms of the MIT License; see LICENSE file for more
 // details.
 
 import React, { Component } from "react";
-import { FieldArray } from "formik";
+import _get from "lodash/get";
+import { getIn } from "formik";
 import PropTypes from "prop-types";
 import { Button, Form, Icon } from "semantic-ui-react";
 import { ArrayField, GroupField } from "react-invenio-forms";
@@ -21,8 +22,15 @@ export class MetadataFields extends Component {
   }
 
   render() {
-    const { fieldPath, errors } = this.props;
-    //const error = getIn(errors, fieldPath, null);
+    const { fieldPath } = this.props;
+    let form = "";
+    const getFieldErrors = (form, fieldPath) => {
+      return (
+        getIn(form.errors, fieldPath, null) ||
+        getIn(form.initialErrors, fieldPath, null)
+      );
+    };
+    const fieldsError = getFieldErrors(form, `${fieldPath}.fields`);
     return (
       <>
         <GroupField fieldPath={`${fieldPath}.leader`} className={"leader"}>
@@ -33,16 +41,28 @@ export class MetadataFields extends Component {
           fieldPath={`${fieldPath}.fields`}
           defaultNewValue={{ id: "", ind1: "", ind2: "", subfield: "$$" }}
           className="marcxml metadata fields."
-          component={(formikProps) => (
-            <MetadataField {...formikProps} {...this.props} />
-          )}
-        ></FieldArray>
+        >
+          {({ arrayHelpers, indexPath }) => {
+            const fieldPathPrefix = `${fieldPath}.fields.${indexPath}`;
+            return (
+              <GroupField optimized>
+                <MetadataField fieldPath={fieldPathPrefix} error={fieldsError}  />
+                <Form.Field width={1}>
+                  <Button icon onClick={() => arrayHelpers.remove(indexPath)}>
+                    <Icon name="close" />
+                  </Button>
+                </Form.Field>
+              </GroupField>
+            );
+          }}
+        </ArrayField>
       </>
     );
   }
 }
 
 MetadataFields.propTypes = {
+  form: PropTypes.object.isRequired,
   fieldPath: PropTypes.string,
   validationSchema: PropTypes.object,
 };
